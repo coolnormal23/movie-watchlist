bfabcb1a8b4621df997c492b29c9f18856790510
Jakob Edgeworth <jakobedgeworth@gmail.com>
2025-12-24 22:08:03 -0600

    finish search functionality, finish movie styling

--- index.js
+++ index.js
@@ -1,94 +1,62 @@
+//Author: Jakob Edgeworth (github:coolnormal23)
+
+//html els
 const form = document.forms[0]
 const main = document.getElementById('main')
 const emptyDiv = document.getElementById('empty')
+const seemore = document.getElementById('seemore')
 const results = document.getElementById('results')
+const noresults = document.getElementById('no-results')
 
+//showing movies and tracking movie state
+let searchState = []
 let i = 0
 const resultsToShow = 3
 
-console.log(form, main)
+async function renderMovies(){
+    let j = i
+    let html = ''
+    for(i; (i < resultsToShow+j && i < 10); i++){
+        console.log(i)
+        //get individual movie data
+        const movieID = searchState[i].imdbID
+        const movieData = await searchMovieByID(movieID)
 
-/*
-    Title
-    Runtime
-    Genres
-    Poster
-    IMDB
-    Description
-*/
+        //update results display
+        main.classList.remove("blank")
+        emptyDiv.style.display = 'none'
+        noresults.style.display = 'none'
+        results.style.display = 'block'
+        seemore.style.display = 'block'
 
-const testObj = {
-  "Title": "Blade Runner",
-  "Year": "1982",
-  "Rated": "R",
-  "Released": "25 Jun 1982",
-  "Runtime": "117 min",
-  "Genre": "Action, Drama, Sci-Fi",
-  "Director": "Ridley Scott",
-  "Writer": "Hampton Fancher, David Webb Peoples, Philip K. Dick",
-  "Actors": "Harrison Ford, Rutger Hauer, Sean Young",
-  "Plot": "A blade runner must pursue and terminate four replicants who stole a ship in space and have returned to Earth to find their creator.",
-  "Language": "English, German, Cantonese, Japanese, Hungarian, Arabic, Korean",
-  "Country": "United States, United Kingdom, Hong Kong",
-  "Awards": "Nominated for 2 Oscars. 13 wins & 22 nominations total",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BOWQ4YTBmNTQtMDYxMC00NGFjLTkwOGQtNzdhNmY1Nzc1MzUxXkEyXkFqcGc@._V1_SX300.jpg",
-  "Ratings": [
-    {
-      "Source": "Internet Movie Database",
-      "Value": "8.1/10"
-    },
-    {
-      "Source": "Rotten Tomatoes",
-      "Value": "89%"
-    },
-    {
-      "Source": "Metacritic",
-      "Value": "84/100"
-    }
-  ],
-  "Metascore": "84",
-  "imdbRating": "8.1",
-  "imdbVotes": "868,708",
-  "imdbID": "tt0083658",
-  "Type": "movie",
-  "DVD": "N/A",
-  "BoxOffice": "$32,914,489",
-  "Production": "N/A",
-  "Website": "N/A",
-  "Response": "True"
-}
-
-results.children[0].innerHTML = `
-    <div class="movie">
-        <div>
-            <img class="poster" src="${testObj.Poster}"></img>
-        </div>
-        <div>
-            <div class="flexcenter">
-                <p>${testObj.Title}</p>
-                <i class="fa-solid fa-star"></i>
-                <p>${testObj.imdbRating}</p>
-            </div>
-            <div class="flexcenter">
-                <p>${testObj.Runtime}</p>
-                <p>${testObj.Genre}</p>
-                <div role="button" class="flexcenter">
-                    <i class="fa-regular fa-square-plus"></i>
-                    <p>Watchlist</p>
+        //render movie
+        console.log(`movie ${i} =`,movieData)
+        html += `
+            <div class="movie">
+                <div class="posterdiv flex center">
+                    <img class="poster" src="${movieData.Poster}"></img>
+                </div>
+                <div class="infodiv">
+                    <div class="flex center">
+                        <p id="title">${movieData.Title}</p>
+                        <i class="fa-solid fa-star star"></i>
+                        <p>${movieData.imdbRating}</p>
+                    </div>
+                    <div class="secondaryinfo">
+                        <p>${movieData.Runtime}</p>
+                        <p>${movieData.Genre}</p>
+                        <div role="button" class="flex center">
+                            <i class="fa-regular fa-square-plus"></i>
+                            <p>Watchlist</p>
+                        </div>
+                    </div>
+                    <p class="description">${movieData.Plot}</p>
                 </div>
             </div>
-            <p>${testObj.Plot}</p>
-        </div>
-    </div>
-`
-
-async function renderMovies(data){
-    let j = i
-    for(i; i < resultsToShow+j; i++){
-        const movieID = data.Search[i].imdbID
-        const movieData = await searchMovieByID(movieID)
-        console.log(movieData)
+        `
     }
+    //push to dom
+    results.children[0].innerHTML += html
 }
 
 async function searchMovieByID(id){
@@ -111,18 +79,35 @@ async function searchMoviesByName(title){
     try {
         const response = await fetch(url, options);
         const data = await response.json();
+        results.children[0].innerHTML = ''
         console.log(data);
-        await renderMovies(data)
+        if(data.Response !== "False"){
+            searchState = data.Search
+            await renderMovies()
+        } else {
+            //if no search, toggle on our no results display
+            results.style.display = 'none'
+            seemore.style.display = 'none'
+            emptyDiv.style.display = 'none'
+            noresults.style.display = 'block'
+        }
     } catch (error) {
         console.error(error);
     }
 }
 
+async function handleSeeMore(e){
+    renderMovies()
+}
+
+
 function handleSearchSubmit(e){
     e.preventDefault()
     if(form[0].value){
+        i = 0 //reset movie counter back to 0
         searchMoviesByName(form[0].value.trimEnd().replaceAll(' ','+'))
     }
 }
 
 form.addEventListener('submit', handleSearchSubmit)
+seemore.addEventListener('click', handleSeeMore)
