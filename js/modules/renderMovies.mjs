bcef4e74debfcdc1572ea299ad946b91cc33cfa6
Jakob Edgeworth <jakobedgeworth@gmail.com>
2025-12-28 00:47:59 -0600

    add footer and more styling

--- js/modules/renderMovies.mjs
+++ js/modules/renderMovies.mjs
@@ -7,11 +7,11 @@ export default async function renderMovies(movies, add){
         //render movie
         html += `
             <div class="movie">
-                <div class="posterdiv flex center">
+                <div class="posterdiv flex aligncenter">
                     <img class="poster" src="${img}"></img>
                 </div>
                 <div class="infodiv">
-                    <div class="flex center">
+                    <div class="flex aligncenter">
                         <p id="title">${movieData.Title}</p>
                         <i class="fa-solid fa-star star"></i>
                         <p>${movieData.imdbRating}</p>
@@ -52,13 +52,13 @@ function movieInWatchlist(movieData, add){
         }
 
         return `
-            <div role="button" class="flex center moviebutton" data-movie="${movieData.imdbID}">
+            <div role="button" class="flex aligncenter moviebutton" data-movie="${movieData.imdbID}">
                 ${'<i class="fa-regular fa-square-plus"></i><p>Watchlist</p>'}
             </div>
         `
     } else {
         return `
-            <div role="button" class="flex center moviebutton" data-movie="${movieData.imdbID}">
+            <div role="button" class="flex aligncenter moviebutton" data-movie="${movieData.imdbID}">
                 ${'<i class="fa-solid fa-circle-minus"></i><p>Remove</p>'}
             </div>
         `
